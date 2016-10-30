034579a9092c3ac1ccb0bb7b5f29f75d99e70647
wingston sharon <wingston.sharon@gmail.com>
2016-10-30 09:22:22 +0400

    adding test scripts

--- dnadriveTest.py
+++ dnadriveTest.py
@@ -6,32 +6,35 @@ class dnadriveTestCase(unittest.TestCase):
 #  Test cases for encode_string
    
    def test_emptyString(self):
-      self.assertIsNotNone(dnadrive.encode_string(""))
+      self.assertRaises(dnadrive.StringEmptyException,dnadrive.encode_string,"")
 
    def test_intInString(self):
-      self.assertIsNone(dnadrive.encode_string(1))
+      self.assertRaises(dnadrive.NotStringException,dnadrive.encode_string,1)
 
    def test_variableInString(self):
       a='abc'
       self.assertIsNotNone(dnadrive.encode_string(a))
 
-#  Test cases for encode_file
+   def test_encodedecode(self):
+      inp = "hello world"
+      gene = dnadrive.encode_string(inp)
+      outp = dnadrive.decode_string(gene)
+      self.assertEqual(inp,outp)
 
-#   def test_emptyFile(self):
-#      self.assertIs
+# #  Test cases for encode_file
 
-#  Test cases for decode_string      
-   def test_emptyDecode(self):
-      self.assertIsNone(dnadrive.decode_string(""))
+# #  Test cases for decode_string      
+#    def test_emptyDecode(self):
+#       self.assertRaises(dnadrive.decode_string(""))
 
-   def test_validString(self):
-      self.assertIsNotNone(dnadrive.decode_string("GGGGGGATATGGGGGG"))
+#    def test_validString(self):
+#       self.assertIsNotNone(dnadrive.decode_string("GGGGGGATATGGGGGG"))
 
-   def test_wrongSequenceType1(self):
-      self.assertIsNone(dnadrive.decode_string("GGGGGGATGGATGGGG"))
+#    def test_wrongSequenceType1(self):
+#       self.assertIsNone(dnadrive.decode_string("GGGGGGATGGATGGGG"))
 
-   def test_wrongSequenceType2(self):
-      self.assertIsNone(dnadrive.decode_string("AKJSDFHAKJSDHAKJSDGH"))
+#    def test_wrongSequenceType2(self):
+#       self.assertIsNone(dnadrive.decode_string("AKJSDFHAKJSDHAKJSDGH"))
 
 if __name__=='__main__':
    unittest.main()
