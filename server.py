e48bd06463b4eb7e9e8453f5c1aa3f06ea8f8a37
Wingston Sharon <wingston.sharon@gmail.com>
2016-11-25 08:46:19 +0400

    adding server variant handling

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from flask import Flask, request, redirect, url_for, send_from_directory,render_
 from werkzeug.utils import secure_filename
 from flask_wtf.csrf import CsrfProtect
 csrf = CsrfProtect()
-import magic
+# import magic
 from dnadrive import dnadrive
-mime = magic.Magic(mime=True)
+# mime = magic.Magic(mime=True)
 UPLOAD_FOLDER = '/tmp/flaskfiles/'
 WTF_CSRF_CHECK_DEFAULT = False
 app = Flask(__name__)
@@ -18,13 +18,14 @@ def home():
 @app.route("/api/encode/", methods=['POST','PUT'])
 @csrf.exempt
 def encode():
+    variant = int(request.form['variant'])
     file = request.files['filedata']
     filename = secure_filename(file.filename)
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(file_path)
     out_filename = filename.replace('.','_')+'.moss'
     out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], out_filename)
-    gene = dnadrive.encode_file(file_path,out_file_path)
+    gene = dnadrive.encode_file(file_path,out_file_path,variant)
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                out_filename, as_attachment=True)
 
@@ -42,4 +43,4 @@ def decode():
                                out_filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
