62a406e3ddc97b7a5120edfc6ca4b96e375ed24e
Wingston Sharon <wingston.sharon@gmail.com>
2016-11-26 10:45:56 +0400

    files update

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory,render_template
+from flask import Flask, request, redirect, url_for, send_from_directory,render_template,jsonify
 from werkzeug.utils import secure_filename
 from flask_wtf.csrf import CsrfProtect
+import hashlib
+m = hashlib.md5()
 csrf = CsrfProtect()
 # import magic
 from dnadrive import dnadrive
@@ -19,31 +21,60 @@ def home():
 @csrf.exempt
 def encode():
     variant = int(request.form['variant'])
-    file = request.files['filedata']
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(file_path)
-    out_filename = filename.replace('.','_')+'.moss'
-    out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], out_filename)
-    gene = dnadrive.encode_file(file_path,out_file_path,variant)
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               out_filename, as_attachment=True)
-
+    if 'filedata' in request.files:
+        file = request.files['filedata']
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        out_filename = filename.replace('.','_')+'.moss'
+        out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], out_filename)
+        dnadrive.encode_file(file_path,out_file_path,variant)
+        return send_from_directory(app.config['UPLOAD_FOLDER'],
+                                   out_filename, as_attachment=True)
+    else:
+        m.update(request.form['textdata'])
+        filename =  m.hexdigest()[:5]
+        filename = filename[:5]
+        # print filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        with open(file_path, "w") as text_file:
+            text_file.write(request.form['textdata'])
+        out_filename = filename.replace('.','_')+'.moss'
+        out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], out_filename)
+        dnadrive.encode_file(file_path,out_file_path,variant)
+        with open(out_file_path, 'r') as myfile:
+            data=myfile.read().replace('\n','</br>')
+        return jsonify({"data":data})
+    
 @csrf.exempt
 @app.route("/api/decode/", methods=['POST','PUT'])
 def decode():
-    file = request.files['filedata']
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(file_path)
-    infile = open(file_path, 'r')
-    firstLine = infile.readline()
-    # out_filename = "ds"
-    out_filename = firstLine.split("|")[0].strip().split('@')[0]
-    out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], out_filename)
-    gene = dnadrive.decode_file(file_path,out_file_path)
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               out_filename, as_attachment=True)
+    if 'filedata' in request.files:
+        file = request.files['filedata']
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        infile = open(file_path, 'r')
+        firstLine = infile.readline()
+        # out_filename = "ds"
+        out_filename = firstLine.split("|")[0].strip().split('@')[0]
+        out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], out_filename)
+        gene = dnadrive.decode_file(file_path,out_file_path)
+        return send_from_directory(app.config['UPLOAD_FOLDER'],
+                                   out_filename, as_attachment=True)
+    else:
+        m.update(request.form['textdata'])
+        filename =  m.hexdigest()[:5]
+        filename = filename[:5]
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        with open(file_path, "w") as text_file:
+            text_file.write(request.form['textdata'].replace('</br>','\n'))
+        out_filename = filename.replace('.','_')+'.moss'
+        out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], out_filename)
+        dnadrive.decode_file(file_path,out_file_path)
+        with open(out_file_path, 'r') as myfile:
+            data=myfile.read().replace('\n','</br>')
+        return jsonify({"data":data})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
