1ac3a0c0d0a6951f4049b7e44d2f938cb920fc1e
wingston sharon <wingston.sharon@gmail.com>
2016-12-22 10:14:14 +0400

    version bump for python 3 compat

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'dnadrive',
   packages = ['dnadrive'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'This Package contains utility wrappers for dnadrive',
   author = 'Wington Sharon',
   author_email = 'wingston@helix.works',
   url = 'https://github.com/wingie/dnadrive', # use the URL to the github repo
-  download_url = 'https://github.com/wingie/dnadrive/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/wingie/dnadrive/tarball/0.2', # I'll explain this in a second
   keywords = ['dnadrive', 'biopython'], # arbitrary keywords
   classifiers = [],
   scripts=['dnadrive/dnadrive'] 
